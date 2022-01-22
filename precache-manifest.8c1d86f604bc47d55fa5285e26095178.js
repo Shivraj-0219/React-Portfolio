4402508dfa766abaa586176e7320a50bf4672847
Shivraj-0219 <shivrajsm0219@gmail.com>
2022-01-22 22:51:13 +0530

    Updates

---
+++ precache-manifest.8c1d86f604bc47d55fa5285e26095178.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb24113e3308981b128df01d8e83537",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5c89fd85e77149e4afe5",
+    "url": "/React-Portfolio/static/css/main.c279d164.chunk.css"
+  },
+  {
+    "revision": "4bc4d2116ffc7de714e1",
+    "url": "/React-Portfolio/static/js/2.c6eac39d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.c6eac39d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c89fd85e77149e4afe5",
+    "url": "/React-Portfolio/static/js/main.52d5ba00.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "fe7a19b8eceda8a510e72e4f210c5c05",
+    "url": "/React-Portfolio/static/media/laptophomepage.fe7a19b8.png"
+  },
+  {
+    "revision": "982e67352daf74d7b07299143354f6a9",
+    "url": "/React-Portfolio/static/media/mobilemode.982e6735.png"
+  },
+  {
+    "revision": "2a6fd761aef0e6f52212246d6f488e60",
+    "url": "/React-Portfolio/static/media/tabmode.2a6fd761.png"
+  },
+  {
+    "revision": "191feb907d94a0993009182be5d1418a",
+    "url": "/React-Portfolio/static/media/work1.191feb90.jpg"
+  },
+  {
+    "revision": "b855399263e7984f81521116704125b1",
+    "url": "/React-Portfolio/static/media/work2.b8553992.jpg"
+  },
+  {
+    "revision": "27dc07a4f0d8b0cc088598f58cd3d94e",
+    "url": "/React-Portfolio/static/media/work3.27dc07a4.jpg"
+  },
+  {
+    "revision": "09b0176f1df222939a6e3a48454a43db",
+    "url": "/React-Portfolio/static/media/work4.09b0176f.jpg"
+  },
+  {
+    "revision": "0087a913655292fe673c3b2e5dce289f",
+    "url": "/React-Portfolio/static/media/work5.0087a913.jpg"
+  },
+  {
+    "revision": "056c87aa6f9e07cdcdbae27cf7651133",
+    "url": "/React-Portfolio/static/media/work6.056c87aa.jpg"
+  }
+]);
